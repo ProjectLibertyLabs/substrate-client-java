3af115307c8222ce0f7b253f86bc1c4fefa4ec26
Peter Frank <>
2025-04-15 10:37:41 -0700

    1357: Now using Frequency 1.16.0

--- rpc/rpc-api/src/test/java/com/strategyobject/substrateclient/rpc/api/SignedExtraTest.java
+++ rpc/rpc-api/src/test/java/com/strategyobject/substrateclient/rpc/api/SignedExtraTest.java
@@ -1,12 +1,9 @@
-package com.strategyobject.substrateclient.api.pallet.balances;
+package com.strategyobject.substrateclient.rpc.api;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 import com.strategyobject.substrateclient.common.convert.HexConverter;
-import com.strategyobject.substrateclient.rpc.api.ImmortalEra;
-import com.strategyobject.substrateclient.rpc.api.SignedAdditionalExtra;
-import com.strategyobject.substrateclient.rpc.api.SignedExtra;
 import com.strategyobject.substrateclient.rpc.api.primitives.BlockHash;
 import com.strategyobject.substrateclient.rpc.api.primitives.Hash256;
 import com.strategyobject.substrateclient.rpc.api.primitives.Index;
@@ -39,7 +36,7 @@ class SignedExtraTest {
    *
    * @param includeMetadataHash whether to include metadata hash in the SignedExtra
    * @param expectedHash the expected hex output of the SCALE data
-   * @throws Exception if an error occurs during serialization
+   * @throws ExBalancesTestception if an error occurs during serialization
    */
   @ParameterizedTest
   @CsvSource({ "false,0x000000", "true,0x00000000" })
@@ -69,8 +66,8 @@ class SignedExtraTest {
   @ParameterizedTest
   @CsvSource(
     {
-      "false,0x8d0000000100000077000000000000000000000000000000000000000000000000000000000000777700000000000000000000000000000000000000000000000000000000000077",
-      "true,0x8d000000010000007700000000000000000000000000000000000000000000000000000000000077770000000000000000000000000000000000000000000000000000000000007700",
+      "false,0x970000000100000077000000000000000000000000000000000000000000000000000000000000777700000000000000000000000000000000000000000000000000000000000077",
+      "true,0x97000000010000007700000000000000000000000000000000000000000000000000000000000077770000000000000000000000000000000000000000000000000000000000007700",
     }
   )
   void testSignedExtraAdditional(boolean includeMetadataHash, String expectedHash) throws Exception {
