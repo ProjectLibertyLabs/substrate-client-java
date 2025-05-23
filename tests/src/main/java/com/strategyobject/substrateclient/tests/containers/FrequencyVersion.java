129e530ab53ff96a243c4c2d4e6db06965ddf905
tedward111 <17606711+tedward111@users.noreply.github.com>
2025-05-23 15:28:37 -0400

    Testing with updated Frequency version

--- tests/src/main/java/com/strategyobject/substrateclient/tests/containers/FrequencyVersion.java
+++ tests/src/main/java/com/strategyobject/substrateclient/tests/containers/FrequencyVersion.java
@@ -1,7 +1,7 @@
 package com.strategyobject.substrateclient.tests.containers;
 
 public class FrequencyVersion {
-    public static final String CURRENT_VERSION = "v1.16.1";
+    public static final String CURRENT_VERSION = "v1.16.2";
     public static final Long SPEC = 153L;
 
     private FrequencyVersion() {
