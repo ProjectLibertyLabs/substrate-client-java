cb3f07f8e9dd081ff338f94c3b9c63438540ae9d
Peter Frank <>
2025-09-17 14:35:02 -0700

    Now using Frequency 1.17.4

--- tests/src/main/java/com/strategyobject/substrateclient/tests/containers/FrequencyVersion.java
+++ tests/src/main/java/com/strategyobject/substrateclient/tests/containers/FrequencyVersion.java
@@ -1,8 +1,8 @@
 package com.strategyobject.substrateclient.tests.containers;
 
 public class FrequencyVersion {
-    public static final String CURRENT_VERSION = "v1.17.2";
-    public static final Long SPEC = 169L;
+    public static final String CURRENT_VERSION = "v1.17.4";
+    public static final Long SPEC = 176L;
 
     private FrequencyVersion() {
     }
