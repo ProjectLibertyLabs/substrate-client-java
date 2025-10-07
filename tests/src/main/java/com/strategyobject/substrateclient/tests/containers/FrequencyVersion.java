119064f7d908ea73eaccecc73d810e0acad2ba61
Aziz <aziz.abada@unfinished.com>
2025-10-07 15:50:02 -0700

    upgrading to frequency v1.17.5

--- tests/src/main/java/com/strategyobject/substrateclient/tests/containers/FrequencyVersion.java
+++ tests/src/main/java/com/strategyobject/substrateclient/tests/containers/FrequencyVersion.java
@@ -1,8 +1,8 @@
 package com.strategyobject.substrateclient.tests.containers;
 
 public class FrequencyVersion {
-    public static final String CURRENT_VERSION = "v1.17.4";
-    public static final Long SPEC = 176L;
+    public static final String CURRENT_VERSION = "v1.17.5";
+    public static final Long SPEC = 182L;
 
     private FrequencyVersion() {
     }
