37aceb3985dd35dd172488595c9c65e7dd8b9605
Julian Fortune <julian.fortune@projectliberty.io>
2026-02-06 14:14:27 -0800

    Adds `AccountIdDecoder`

---
+++ rpc/rpc-api/src/main/java/com/strategyobject/substrateclient/rpc/api/AccountIdDecoder.java
@@ -0,0 +1,24 @@
+package com.strategyobject.substrateclient.rpc.api;
+
+import com.strategyobject.substrateclient.crypto.ss58.SS58Codec;
+import com.strategyobject.substrateclient.rpc.DecoderPair;
+import com.strategyobject.substrateclient.rpc.RpcDecoder;
+import com.strategyobject.substrateclient.rpc.annotation.AutoRegister;
+import com.strategyobject.substrateclient.rpc.context.RpcDecoderContext;
+import com.strategyobject.substrateclient.transport.RpcObject;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@AutoRegister(types = AccountId.class)
+public class AccountIdDecoder implements RpcDecoder<AccountId> {
+    private final @NonNull RpcDecoderContext rpcDecoderContext;
+
+    @Override
+    public AccountId decode(RpcObject value, DecoderPair<?>... decoders) {
+        if (value.isNull()) { return null; }
+        if (decoders != null && decoders.length > 0) throw new IllegalArgumentException();
+
+        return AccountId.fromBytes(SS58Codec.decode(value.asString()).getAddress());
+    }
+}
