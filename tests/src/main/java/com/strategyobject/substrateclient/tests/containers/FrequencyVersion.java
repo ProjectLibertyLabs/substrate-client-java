4a285ea103835a09e245bcdc90a89522f5e5972a
Peter Frank <peter.frank@projectliberty.io>
2026-03-09 20:34:33 -0700

    Now using Frequency 2.0.1

--- tests/src/main/java/com/strategyobject/substrateclient/tests/containers/FrequencyVersion.java
+++ tests/src/main/java/com/strategyobject/substrateclient/tests/containers/FrequencyVersion.java
@@ -1,8 +1,8 @@
 package com.strategyobject.substrateclient.tests.containers;
 
 public class FrequencyVersion {
-    public static final String CURRENT_VERSION = "v2.0.0-rc1";
-    public static final Long SPEC = 184L;
+    public static final String CURRENT_VERSION = "v2.0.1";
+    public static final Long SPEC = 189L;
 
     private FrequencyVersion() {
     }
